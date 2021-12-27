eb82235edb64c14772951ced60cf7d3d19b86db9
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2021-12-27 23:49:34 +0300

    Add files via upload

---
+++ c4.js
@@ -0,0 +1,19 @@
+let DolScore = (96+108+89)/3;
+console.log(`the score is ${DolScore}`);
+
+
+let KolaScore= (88+ 91 + 110)/3
+
+console.log(`the score is ${KolaScore}`);
+
+
+if (DolScore> KolaScore ){
+    console.log(`dolphin score is bigger, it wins, because the score is ${DolScore}`);
+}
+else if (KolaScore>DolScore  ){
+    console.log(`kola score is bigger, it wins, because the score is ${KolaScore}`);
+}
+else if (DolScore === KolaScore) {
+    console.log(`kola score and dolphin scores are equal`);
+
+}
